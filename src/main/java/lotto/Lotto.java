f626ca5f21dd410e476c3d1b0c43a2173e669dc8
Yoon <kohyc97@gmail.com>
2023-11-07 16:23:03 +0900

    feat: 랜덤 로또번호 생성 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,7 @@
 package lotto;
 
 import java.util.List;
-
+import camp.nextstep.edu.missionutils.Console;
 public class Lotto {
     private final List<Integer> numbers;
 
@@ -12,8 +12,14 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6자리 입니다");
         }
+    }
+
+    public List getLottoNumber() {
+        String lottoNumber = Console.readLine();
+
+        return ;
     }
 
     // TODO: 추가 기능 구현
---
+++ src/main/java/lotto/RandomLotto.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RandomLotto {
+    public RandomLotto(){}
+
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 1;
+    private static final int COUNT_NUMBER = 6;
+    private static List<Integer> RandomLottoNumbers;
+
+    public static List<Integer> getRandomLottoNumbers() {
+        RandomLottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, COUNT_NUMBER);
+        List<Integer> lottoTicketNumberList = new ArrayList<>(RandomLottoNumbers);
+        Collections.sort(lottoTicketNumberList);
+        return lottoTicketNumberList;
+    }
+
+}
