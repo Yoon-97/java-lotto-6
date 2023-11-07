c82f67512d656068380311afbcc11da3386d22b9
Yoon <kohyc97@gmail.com>
2023-11-07 18:13:39 +0900

    refactor: 함수들 안내문구 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -30,6 +30,7 @@ public class Lotto {
     }
     public List<Integer> getLottoNumber() {
         List<Integer> lottoNumber = new ArrayList<>();
+        System.out.println("당첨 번호를 입력해 주세요.");
         for (String number : Console.readLine().split(",")) {
             if (!Character.isDigit(number.charAt(0))) {
                 throw new IllegalArgumentException("[ERROR] 유효한 숫자를 입력해주세요");
@@ -39,11 +40,25 @@ public class Lotto {
         return lottoNumber;
     }
 
+    public int getBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        for (char i : input.toCharArray()) {
+            if (!Character.isDigit(i)) {
+                throw new IllegalArgumentException("유효한 숫자를 입력해주세요");
+            }
+        }
+        round = Integer.parseInt(input);
+    }
+
     public int buyLotto(int price) {
+        System.out.println("구입금액을 입력해 주세요.");
         if (price % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1,000원 단위로 구입하여야 합니다.");
         }
+        int countLotto = price / 1000
+        System.out.println(countLotto + "개를 구매했습니다.");
         //구매한 로또 갯수 리턴
-        return price / 1000;
+        return countLotto;
     }
 }
